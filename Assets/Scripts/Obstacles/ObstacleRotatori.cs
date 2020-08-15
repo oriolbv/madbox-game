a63b6c664bd74b21574d17234662e0fa9bcb0c12
Oriol Burgaya <oriolbv@gmail.com>
2020-08-15 10:18:26 +0200

    ObstacleRotatori implemented

---
+++ Assets/Scripts/Obstacles/ObstacleRotatori.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleRotatori : MonoBehaviour
+{
+    [Header("Physics")]
+    public float SpinSpeed = 40f;
+    public bool IsClockwise = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int clockwise = (IsClockwise) ? 1 : -1;
+        transform.Rotate(0, clockwise * SpinSpeed * Time.deltaTime, 0);
+    }
+}
