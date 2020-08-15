e2bd75e08706e1b900970784578daaf2fb74014d
Oriol Burgaya <oriolbv@gmail.com>
2020-08-15 10:10:07 +0200

    All ObstaclesHorizontal added into scene and ExtendedBehaviour script

---
+++ Assets/Scripts/ObstaclesMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesMovement : MonoBehaviour
+{
+    public GameObject[] MovePoints;
+    public GameObject Player;
+    int current = 0;
+    public float speed;
+    float WPradius = 1;
+
+    void Update()
+    {
+        if (Vector3.Distance(MovePoints[current].transform.position, transform.position) < WPradius)
+        {
+            current = Random.Range(0, MovePoints.Length);
+            if (current >= MovePoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, MovePoints[current].transform.position, Time.deltaTime * speed);
+
+    }
+
+    //void OnTriggerEnter(Collider n)
+    //{
+    //    if (n.gameObject == player)
+    //    {
+    //        player.transform.parent = transform;
+    //    }
+    //}
+    //void OnTriggerExit(Collider n)
+    //{
+    //    if (n.gameObject == player)
+    //    {
+    //        player.transform.parent = null;
+    //    }
+    //}
+}
